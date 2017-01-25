f420464b6c7cdaec215afdff1bb0d57f93bf9862
pablo-pacheco <pablo.pacheco@origgami.com.br>
2017-01-25 10:57:32 -0200

    passing an icon class to thumb buttons

--- templates/thumb-button.php
+++ templates/thumb-button.php
@@ -9,17 +9,15 @@
  * @since   1.0.0
  */
 
-//error_log( print_r( $params,true ) );
-
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 ?>
 
 <div data-item_id="<?php echo get_the_ID(); ?>" data-action="<?php echo esc_attr( $params['btn_data_action'] ); ?>" class="<?php echo esc_attr( $params['btn_class'] ); ?>">
 	<div class="alg-wc-wl-view-state alg-wc-wl-view-state-add">
-		<i class="fa fa-heart" aria-hidden="true"></i>
+		<i class="<?php echo esc_attr( $params['btn_icon_class'] );?>" aria-hidden="true"></i>
 	</div>
 	<div class="alg-wc-wl-view-state alg-wc-wl-view-state-remove">
-		<i class="fa fa-heart" aria-hidden="true"></i>
+		<i class="<?php echo esc_attr( $params['btn_icon_class'] );?>" aria-hidden="true"></i>
 	</div>
 	<i class="loading fa fa-refresh fa-spin fa-fw"></i>
 </div>

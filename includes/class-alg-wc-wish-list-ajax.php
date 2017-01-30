5dc9e9317ff415306739d1ace6d93dcd5843bc7a
pablo-pacheco <pablo.pacheco@origgami.com.br>
2017-01-30 17:23:24 -0200

    Added an param to (alg_wc_wl_toggle_item_texts) filter to override 'see your wish list' text

--- includes/class-alg-wc-wish-list-ajax.php
+++ includes/class-alg-wc-wish-list-ajax.php
@@ -33,9 +33,10 @@ if ( ! class_exists( 'Alg_WC_Wish_List_Ajax' ) ) {
 			$action = 'added'; // 'added' | 'removed' | error
 
 			$params = apply_filters( 'alg_wc_wl_toggle_item_texts', array(
-				'added'   => __( '%s was successfully added to wish list', 'alg-wish-list-for-woocommerce' ),
-				'removed' => __( '%s was successfully removed from wish list', 'alg-wish-list-for-woocommerce' ),
-				'error'   => __( 'Sorry, Some error ocurred. Please, try again later.', 'alg-wish-list-for-woocommerce' )
+				'added'         => __( '%s was successfully added to wish list', 'alg-wish-list-for-woocommerce' ),
+				'removed'       => __( '%s was successfully removed from wish list', 'alg-wish-list-for-woocommerce' ),
+				'see_wish_list' => __( 'See your wish list', 'alg-wish-list-for-woocommerce' ),
+				'error'         => __( 'Sorry, Some error ocurred. Please, try again later.', 'alg-wish-list-for-woocommerce' )
 			) );
 
 			if ( ! is_user_logged_in() ) {
@@ -47,24 +48,24 @@ if ( ! class_exists( 'Alg_WC_Wish_List_Ajax' ) ) {
 
 			if ( $response === false ) {
 				$message = __( $params['error'], 'alg-wish-list-for-woocommerce' );
-				$all_ok = false;
-				$action = 'error';
+				$all_ok  = false;
+				$action  = 'error';
 			} elseif ( $response === true ) {
-				$message = sprintf (
+				$message = sprintf(
 					__( $params['removed'], 'alg-wish-list-for-woocommerce' ),
-					'<b>'.$product->get_title().'</b>'
+					'<b>' . $product->get_title() . '</b>'
 				);
-				$action = 'removed';
+				$action  = 'removed';
 			} elseif ( is_numeric( $response ) ) {
-				$wish_list_page_id = Alg_WC_Wish_List_Page::get_wish_list_page_id();
-				$wish_list_permalink = get_permalink($wish_list_page_id);
-				$see_your_wishlist_message = __('See your wish list', 'alg-wish-list-for-woocommerce' );
-				$added_message = sprintf (
+				$wish_list_page_id         = Alg_WC_Wish_List_Page::get_wish_list_page_id();
+				$wish_list_permalink       = get_permalink( $wish_list_page_id );
+				$see_your_wishlist_message = __( $params['removed'], 'alg-wish-list-for-woocommerce' );
+				$added_message             = sprintf(
 					__( $params['added'], 'alg-wish-list-for-woocommerce' ),
-					'<b>'.$product->get_title().'</b>'
+					'<b>' . $product->get_title() . '</b>'
 				);
-				$message = __( "{$added_message}<br /> <a class='alg-wc-wl-notification-link' href='{$wish_list_permalink}'>{$see_your_wishlist_message}</a>", 'alg-wish-list-for-woocommerce' );
-				$action = 'added';
+				$message                   = __( "{$added_message}<br /> <a class='alg-wc-wl-notification-link' href='{$wish_list_permalink}'>{$see_your_wishlist_message}</a>", 'alg-wish-list-for-woocommerce' );
+				$action                    = 'added';
 			}
 
 			$response = array( 'message' => $message, 'action' => $action );

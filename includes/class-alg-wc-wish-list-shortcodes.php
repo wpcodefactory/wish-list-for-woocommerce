8e95edbbdcb851d1e39094ca6f78cb11bf6316ef
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2022-10-10 19:48:05 -0300

    Test

--- includes/class-alg-wc-wish-list-shortcodes.php
+++ includes/class-alg-wc-wish-list-shortcodes.php
@@ -27,13 +27,12 @@ if ( ! class_exists( 'Alg_WC_Wish_List_Shortcodes' ) ) {
 		 */
 		public static function sc_alg_wc_wl_counter( $atts ) {
 			$atts = shortcode_atts( array(
-				'ignore_excluded_items' => false,
+				'ignore_excluded_items' => 'false',
 				'amount' => false,
 			), $atts, self::SHORTCODE_WISH_LIST_COUNT );
+			$ignore_excluded_items = filter_var( $atts['ignore_excluded_items'], FILTER_VALIDATE_BOOLEAN );
 
 			if ( empty( $atts['amount'] ) ) {
-				$atts['ignore_excluded_items'] = filter_var( $atts['ignore_excluded_items'], FILTER_VALIDATE_BOOLEAN );
-
 				$user_id_from_query_string = get_query_var( Alg_WC_Wish_List_Query_Vars::USER, null );
 				$user_id                   = ! empty( $user_id_from_query_string ) ? Alg_WC_Wish_List_Query_Vars::crypt_user( $user_id_from_query_string, 'd' ) : null;
 				$use_id_from_unlogged_user = filter_var( get_query_var( Alg_WC_Wish_List_Query_Vars::USER_UNLOGGED, false ), FILTER_VALIDATE_BOOLEAN );
@@ -42,7 +41,7 @@ if ( ! class_exists( 'Alg_WC_Wish_List_Shortcodes' ) ) {
 					$user_id = $user->ID;
 				}
 
-				$wishlisted_items = Alg_WC_Wish_List::get_wish_list( $user_id, $use_id_from_unlogged_user, $atts['ignore_excluded_items'] );
+				$wishlisted_items = Alg_WC_Wish_List::get_wish_list( $user_id, $use_id_from_unlogged_user, $ignore_excluded_items );
 				$amount = is_array( $wishlisted_items ) ? count( $wishlisted_items ) : 0;
 			} else {
 				$amount = $atts['amount'];
